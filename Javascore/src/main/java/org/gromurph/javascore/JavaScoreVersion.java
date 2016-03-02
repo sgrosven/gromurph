9bdd5c02da6258dd24545f2143ff751c8090bd02
Sandy Grosvenor <sandy@gromurph.org>
2016-03-02 17:13:16 -0500

    update to izpack 5, and 'ant' does izpack release management, 'gradle' does day-to-day build management

--- Javascore/src/main/java/org/gromurph/javascore/JavaScoreVersion.java
+++ Javascore/src/main/java/org/gromurph/javascore/JavaScoreVersion.java
@@ -2,7 +2,7 @@ package org.gromurph.javascore;
 
 public class JavaScoreVersion 
 {
-	public static final String RELEASE = "2 April 2015";
-    public static final String VERSION = "7.3.1";
+	public static final String RELEASE = "31 March 2016";
+    public static final String VERSION = "7.2016.1";
     // auto updated by build.gradle
 }
