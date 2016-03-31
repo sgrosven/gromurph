cc1389a215bd3af808a47d3fe32b33482155674f
Sandy Grosvenor <sandy@gromurph.org>
2016-03-31 09:08:57 -0400

    7.5 beta 2 packaging for Linda

--- Javascore/src/main/java/org/gromurph/javascore/JavaScoreVersion.java
+++ Javascore/src/main/java/org/gromurph/javascore/JavaScoreVersion.java
@@ -3,6 +3,6 @@ package org.gromurph.javascore;
 public class JavaScoreVersion 
 {
 	public static final String RELEASE = "31 March 2016";
-    public static final String VERSION = "7.2016.1";
+    public static final String VERSION = "7.5 beta 2";
     // auto updated by build.gradle
 }
