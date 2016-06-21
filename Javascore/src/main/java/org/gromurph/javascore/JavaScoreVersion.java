6242666d9b513ed6571d168788579447fbef6095
Sandy Grosvenor <sandy@gromurph.org>
2016-06-21 14:41:58 -0400

    public release 7.5.1

--- Javascore/src/main/java/org/gromurph/javascore/JavaScoreVersion.java
+++ Javascore/src/main/java/org/gromurph/javascore/JavaScoreVersion.java
@@ -4,5 +4,5 @@ public class JavaScoreVersion
 {
 	public static final String RELEASE = "22 June 2016";
     public static final String VERSION = "7.5.1";
-    //must manually update this now
+    // auto updated by build.gradle
 }
