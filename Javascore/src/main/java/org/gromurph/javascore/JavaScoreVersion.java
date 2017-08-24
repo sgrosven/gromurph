416d385469424015c2bad2aa004c32924249f1a0
Sandy Grosvenor <sandy@gromurph.org>
2017-08-24 10:43:45 -0700

    7.5.5 release numbering

--- Javascore/src/main/java/org/gromurph/javascore/JavaScoreVersion.java
+++ Javascore/src/main/java/org/gromurph/javascore/JavaScoreVersion.java
@@ -2,7 +2,7 @@ package org.gromurph.javascore;
 
 public class JavaScoreVersion 
 {
-	public static final String RELEASE = "1 Sept 2017";
+	public static final String RELEASE = "17 Jul 2017";
     public static final String VERSION = "7.5.5";
     // auto updated by build.gradle
 }
